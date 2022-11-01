2623c13b8e131e666099cf8b1b6250478be27f94
Lee <leeuklee@gmail.com>
2022-11-01 20:05:11 +0100

    updated

--- wifi_reset.py
+++ wifi_reset.py
@@ -1,18 +1,8 @@
-import shutil
 import paramiko
-from os import path
-from env import *
+from env import user, password, host, reboot_command
 
 
 def restart_wifi():
-    if i_am == "pi":
-        path_to_file = "/home/pi/.ssh/{}".format(file_name)
-    else:
-        path_to_file = "/Users/{}/.ssh/{}".format(i_am, file_name)
-
-    if not path.exists(file_name):
-        shutil.copy(path_to_file, ".")
-
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh.connect(host, username=user, password=password, timeout=10)
